1aa70a10ba508dd673d037146a168401ce62c533
MAROUANE PONDIKPA <79520932+maxfortune93@users.noreply.github.com>
2024-06-08 18:26:49 -0300

    Initial project setup and implementation of normaliser class

---
+++ src/main/java/org/example/Normaliser.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Normaliser {
+
+
+}
